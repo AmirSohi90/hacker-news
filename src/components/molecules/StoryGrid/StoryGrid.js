59ed1d94fd3f2061c068f9374544f0afb910ff3e
Amir Sohi <amirasohi@gmail.com>
2020-01-08 20:01:11 +0000

    Created story grid that expands

---
+++ src/components/molecules/StoryGrid/StoryGrid.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Story from "../Story/Story";
+import "./StoryGrid.css";
+
+const StoryGrid = ({ stories }) => (
+  <div className="grid-wrapper">
+    {stories.map(story => (
+      <Story
+        timestamp={story.timestamp}
+        title={story.title}
+        score={story.score}
+        author={story.author}
+        text={story.text}
+        url={story.url}
+        key={story.id}
+      />
+    ))}
+  </div>
+);
+
+export default StoryGrid;
